90d3e2f844762d3c58bcf2fdc347227b3a3e8e3a
AJ Foster <web@aj-foster.com>
2018-01-31 23:31:45 -0500

    Add report file writer

--- util/writer.js
+++ util/writer.js
@@ -5,6 +5,9 @@ const outputDir = 'output'
 const jsonFile = function () {
   return outputDir + '/' + global.org + '.json'
 }
+const reportFile = function () {
+  return outputDir + '/' + global.org + '.html'
+}
 
 exports.setup = function () {
   if (!fs.existsSync(outputDir)) {
@@ -18,4 +21,17 @@ exports.writeJSON = function (data) {
   fs.writeFileSync(jsonFile(), JSON.stringify(data))
 }
 
+exports.readJSON = function () {
+  L.debug('Reading saved output, ' + jsonFile())
+
+  const data = fs.readFileSync(jsonFile(), 'utf8')
+  return JSON.parse(data)
+}
+
+exports.writeReport = function (report) {
+  L.debug('Writing report file, ' + reportFile())
+  fs.writeFileSync(reportFile(), report)
+}
+
 exports.jsonFile = jsonFile
+exports.reportFile = reportFile
